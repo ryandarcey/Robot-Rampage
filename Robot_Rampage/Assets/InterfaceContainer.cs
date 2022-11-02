b6e26f2b3cb370a7ee1e15fd4965b4cabbdb5fca
Ryan Darcey <rtdarcey@wpi.edu>
2022-11-02 16:56:10 -0400

    Started level generation

---
+++ Robot_Rampage/Assets/InterfaceContainer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRoom
+{
+	// Properties a room needs:
+	//	walls
+	//	doorways/connections (
+	//	
+
+	// Methods a room needs:
+	// getConnectionByIndex(int i)
+	// 
+}
