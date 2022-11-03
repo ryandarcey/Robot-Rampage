c4904a0c758f105ee26c43f2ad9dd01f72a4a0c1
Ryan Darcey <rtdarcey@wpi.edu>
2022-11-03 17:58:37 -0400

    continued work on level gen
    changed prefab, added script

--- Robot_Rampage/Assets/InterfaceContainer.cs
+++ Robot_Rampage/Assets/InterfaceContainer.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public interface IRoom
 {
 	// Properties a room needs:
-	//	walls
-	//	doorways/connections (
-	//	
+	//	model
+	//GameObject roomModel { get; set; }
+	//	roomSize --> size of model in Unity units (width x length x height from the perspective of the down wall?)
+	Vector3 roomSize { get; set; }
+	//	doorways/connections (objects/structs that store position, orientation)
+
+	//	connectionsArr --> stores booleans (whether/not a connection has another room connected)
 
 	// Methods a room needs:
-	// getConnectionByIndex(int i)
-	// 
+	//	getConnectionByIndex(int i)
+	//	rotateRoom(quarternion?)
+	//	moveRoom(Vector3 position)
+	//	
 }
---
+++ Robot_Rampage/Assets/SquareRoom.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareRoom : MonoBehaviour, IRoom
+{
+    Vector3 IRoom.roomSize { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
