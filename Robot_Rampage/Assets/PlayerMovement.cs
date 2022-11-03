67c1de4afbded3f79db875e408989ffc2db31e03
s3an0 <55671871+s3an0@users.noreply.github.com>
2022-11-03 16:58:45 -0400

    Started a basic room for testing

--- Robot_Rampage/Assets/PlayerMovement.cs
+++ Robot_Rampage/Assets/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     // Layer for checking ground collisions
     public LayerMask groundLayer;
+/*    // Camera attatched to player
+    public Camera camera;
+    // Checks if camera is in first-person or third-person view
+    public bool isFirstPerson = true;*/
 
     // Public attributes for player movement
     public float walkSpeed = 15f;
@@ -61,40 +65,11 @@ public class PlayerMovement : MonoBehaviour
         verticalVelocity.y += gravity * Time.deltaTime;
         controller.Move(verticalVelocity * Time.deltaTime);
 
-        /*        updateGravity();
-                updateMove();*/
-    }
+/*        // Checks if player tried to change 
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            isFirstPerson = !isFirstPerson;
+        }*/
 
-/*    private void updateGravity()
-    {
-        // If the player is supposed to be grounded, set the velocity to a low value to force the player to the ground
-        if(velocity.y < 0 && isGrounded)
-        {
-            velocity.y = -.1f;
-        }
-
-        // If the player is grounded and presses the Jump button, increase velocity by set jumpHeight
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = jumpHeight;
-        }
-
-        // Update player's position based on gravity, independent of framerate
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
 
     }
-
-    private void updateMove()
-    {
-        // Get forward/backward movement (zMovement) and left/right movement (xMovement)
-        float xMovement = Input.GetAxis("Horizontal");
-        float zMovement = Input.GetAxis("Vertical");
-
-        // Set the vector for movement based on xMovement and zMovement
-        Vector3 movement = (xMovement * transform.right) + (zMovement * transform.forward);
-
-        // Move the player independent of the framerate
-        controller.Move(movement * walkSpeed * Time.deltaTime);
-    }*/
 }
